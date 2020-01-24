b9210de8b7a9fe8665b29cbd4ac1bec2b42eb6b6
ryanbriscall <ryanbriscall@gmail.com>
2020-01-24 10:11:56 -0700

    changes http protocol (for link and script assets) to relative.

--- demo/index.php
+++ demo/index.php
@@ -7,7 +7,7 @@ exit;
 
 <head>
 <title>Wizardacious Demo</title>
-<link href="http://getbootstrap.com/2.3.2/assets/css/bootstrap.css" rel="stylesheet">
+<link href="//getbootstrap.com/2.3.2/assets/css/bootstrap.css" rel="stylesheet">
 <link href="../src/css/wizardacious.css" rel="stylesheet">
 <link href="assets/css/demo.css" rel="stylesheet">
 </head>
@@ -26,7 +26,7 @@ exit;
     <p>Copyright&copy; Ryan Briscall</p>
 </footer>
 
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.0/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.0/jquery.min.js"></script>
 <script src="assets/js/jquery.maskedinput.min.js"></script>
 <script src="assets/js/jquery.validate.js"></script>
 
